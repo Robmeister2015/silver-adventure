16459fb279d17d95a676d47cc9c9484d98d46359
Robmeister2015 <A00226084@student.ait.ie>
2016-05-14 16:05:11 +0100

    sdfsd

---
+++ src/test/java/com/rob/movies/MovieWSTest.java
@@ -0,0 +1,14 @@
+package com.rob.movies;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MovieWSTest {
+
+	@Test
+	public void test() {
+		assertEquals(2, 2);
+	}
+
+}
