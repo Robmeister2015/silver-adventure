2f89276fbb146fd7100f0e135e7d2dee45e48e1a
Robmeister20134 <A00226084@student.ait.ie>
2016-05-15 22:33:41 +0100

    pmd errors fixed

--- src/main/java/com/rob/movies/MovieWS.java
+++ src/main/java/com/rob/movies/MovieWS.java
@@ -1,6 +1,5 @@
 package com.rob.movies;
 
-import java.net.URLDecoder;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -14,9 +13,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
 @Path("/movies")
